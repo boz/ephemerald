6447e55eac8aaf5a22f524d8649de763e682e264
Adam Bozanich <adam.boz@gmail.com>
2017-04-26 18:46:11 -0700

    cleanup: poolset

--- poolset.go
+++ poolset.go
@@ -32,6 +32,8 @@ func NewPoolSet(log logrus.FieldLogger, ctx context.Context, configs []*config.C
 
 	var err error
 	var pool Pool
+
+	// create pools
 	for _, cfg := range configs {
 		pool, err = NewPoolWithContext(ctx, cfg)
 		if err != nil {
@@ -40,55 +42,42 @@ func NewPoolSet(log logrus.FieldLogger, ctx context.Context, configs []*config.C
 		pools[cfg.Name] = pool
 	}
 
-	if err != nil {
-		var wg sync.WaitGroup
-		wg.Add(len(pools))
-		for _, pool := range pools {
-			go func(pool Pool) {
-				defer wg.Done()
-				pool.Stop()
-			}(pool)
-		}
-		wg.Wait()
-		return nil, err
+	if err == nil {
+		return &poolSet{
+			pools: pools,
+			log:   log,
+		}, nil
 	}
 
-	return &poolSet{
-		pools: pools,
-		log:   log,
-	}, nil
+	// if creation failed for any pool, stop active pools
+	var wg sync.WaitGroup
+	wg.Add(len(pools))
+	for _, pool := range pools {
+		go func(pool Pool) {
+			defer wg.Done()
+			pool.Stop()
+		}(pool)
+	}
+	wg.Wait()
+	return nil, err
 }
 
 func (ps *poolSet) CheckoutWith(ctx context.Context, names ...string) (params.Set, error) {
-	type pentry struct {
-		name string
-		p    params.Params
-		err  error
+	type pscheckout struct {
+		name   string
+		params params.Params
+		err    error
 	}
 
-	ch := make(chan pentry)
-
+	// checkout from each pool
+	ch := make(chan pscheckout)
 	var wg sync.WaitGroup
-
-	for name, pool := range ps.pools {
-		if len(names) > 0 {
-			found := false
-			for _, n := range names {
-				if name == n {
-					found = true
-					break
-				}
-			}
-			if !found {
-				continue
-			}
-		}
-
+	for name, pool := range ps.poolsForCheckout(names...) {
 		wg.Add(1)
 		go func(name string, pool Pool) {
 			defer wg.Done()
-			p, err := pool.CheckoutWith(ctx)
-			ch <- pentry{name, p, err}
+			params, err := pool.CheckoutWith(ctx)
+			ch <- pscheckout{name, params, err}
 		}(name, pool)
 	}
 
@@ -97,29 +86,26 @@ func (ps *poolSet) CheckoutWith(ctx context.Context, names ...string) (params.Se
 		close(ch)
 	}()
 
+	// collect results
 	set := params.Set{}
-
-	var cerr error
+	errors := make([]error, 0)
 	for entry := range ch {
 		if entry.err != nil {
-			cerr = entry.err
+			ps.log.WithError(entry.err).
+				WithField("pool", entry.name).
+				Error("checkout error")
+			errors = append(errors, entry.err)
 			continue
 		}
-		set[entry.name] = entry.p
+		set[entry.name] = entry.params
 	}
 
-	if cerr != nil {
-		var wg sync.WaitGroup
-		wg.Add(len(set))
-		for name, p := range set {
-			go func(name string, p params.Params) {
-				defer wg.Done()
-				ps.pools[name].Return(p)
-			}(name, p)
-		}
-		wg.Wait()
-		return nil, cerr
+	// if any checkout failed, return completed checkouts
+	if len(errors) > 0 {
+		ps.ReturnAll(set)
+		return nil, errors[0]
 	}
+
 	return set, nil
 }
 
@@ -130,14 +116,12 @@ func (ps *poolSet) Checkout(names ...string) (params.Set, error) {
 func (ps *poolSet) ReturnAll(set params.Set) {
 	var wg sync.WaitGroup
 	wg.Add(len(set))
-
 	for name, p := range set {
 		go func(name string, p params.Params) {
 			defer wg.Done()
 			ps.Return(name, p)
 		}(name, p)
 	}
-
 	wg.Wait()
 }
 
@@ -148,20 +132,32 @@ func (ps *poolSet) Return(name string, item Item) {
 }
 
 func (ps *poolSet) WaitReady() error {
-	ch := make(chan error)
-	defer close(ch)
-
-	for _, pool := range ps.pools {
-		go func(pool Pool) {
-			ch <- pool.WaitReady()
-		}(pool)
+	type pswait struct {
+		name string
+		pool Pool
+		err  error
 	}
 
+	ch := make(chan pswait)
+
+	// call WaitReady on all pools
+	for name, pool := range ps.pools {
+		go func(name string, pool Pool) {
+			ch <- pswait{name, pool, pool.WaitReady()}
+		}(name, pool)
+	}
+
+	// collect results
 	var err error
 	for count := 0; count < len(ps.pools); count++ {
-		if perr := <-ch; perr != nil {
+		if response := <-ch; response.err != nil {
+
+			ps.log.WithError(response.err).
+				WithField("pool", response.name).
+				Error("WaitReady failed")
+
 			if err == nil {
-				err = perr
+				err = response.err
 			}
 		}
 	}
@@ -172,6 +168,7 @@ func (ps *poolSet) Stop() error {
 	var wg sync.WaitGroup
 	wg.Add(len(ps.pools))
 
+	// stop all pools
 	for name, pool := range ps.pools {
 		go func(name string, pool Pool) {
 			defer wg.Done()
@@ -179,6 +176,23 @@ func (ps *poolSet) Stop() error {
 		}(name, pool)
 	}
 
+	// wait for all pools to stop
 	wg.Wait()
 	return nil
+}
+
+func (ps *poolSet) poolsForCheckout(names ...string) map[string]Pool {
+	// if no names given, all pools returned
+	if len(names) == 0 {
+		return ps.pools
+	}
+
+	// else select the pools by name
+	pools := make(map[string]Pool)
+	for _, name := range names {
+		if pool, ok := ps.pools[name]; ok {
+			pools[name] = pool
+		}
+	}
+	return pools
 }
