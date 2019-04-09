8735073f5e8976154e4e630280e8ae7709f51f42
Adam Bozanich <adam.boz@gmail.com>
2019-04-08 22:12:47 -0700

    checkpt

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -2,22 +2,72 @@ package scheduler
 
 import (
 	"context"
+	"io"
+	"io/ioutil"
 
+	"github.com/docker/distribution/reference"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/opencontainers/go-digest"
 )
 
 type Scheduler interface {
-	ResolveImage(context.Context, string) (types.ImageInspect, error)
+	ResolveImage(context.Context, string) (reference.Canonical, error)
+}
+
+func New() Scheduler {
+	return &scheduler{}
 }
 
 type scheduler struct {
+	dc *client.Client
 }
 
-func (s *scheduler) ResolveImage(ctx context.Context, name string) (types.ImageInspect, error) {
-	// ImageInspectWithRaw
+func (s *scheduler) ResolveImage(ctx context.Context, name string) (reference.Canonical, error) {
 
-	// if not present
+	var (
+		ii   types.ImageInspect
+		err  error
+		body io.ReadCloser
+	)
 
-	// ImageCreate
-	return types.ImageInspect{}, nil
+	dc, err := client.NewEnvClient()
+	if err != nil {
+		return nil, err
+	}
+
+	ref, err := reference.ParseNormalizedNamed(name)
+	if err != nil {
+		return nil, err
+	}
+
+	ii, _, err = dc.ImageInspectWithRaw(ctx, ref.String())
+	if err == nil {
+		goto done
+	}
+	if !client.IsErrNotFound(err) {
+		return nil, err
+	}
+
+	body, err = dc.ImagePull(ctx, ref.String(), types.ImagePullOptions{})
+	if err != nil {
+		return nil, nil
+	}
+
+	if _, err := ioutil.ReadAll(body); err != nil {
+		return nil, err
+	}
+
+	ii, _, err = dc.ImageInspectWithRaw(ctx, ref.String())
+	if err != nil {
+		return nil, err
+	}
+
+done:
+	digest, err := digest.Parse(ii.ID)
+	if err != nil {
+		return nil, err
+	}
+	// return reference.WithDigest(ref, digest.Digest(ii.ID))
+	return reference.WithDigest(ref, digest)
 }
---
+++ scheduler/scheduler_test.go
@@ -0,0 +1,34 @@
+package scheduler_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/boz/ephemerald/scheduler"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Scheduler_ResolveImage(t *testing.T) {
+
+	s := scheduler.New()
+
+	obj, err := s.ResolveImage(context.Background(), "nginx:latest")
+	assert.NoError(t, err)
+	t.Logf("%#v\n", obj)
+
+	// t.Log(obj.Name())
+	// nginx        -> docker.io/library/nginx
+	// nginx:latest -> docker.io/library/nginx
+
+	// t.Log(obj.String())
+	// nginx -> docker.io/library/nginx@sha256:cd5239a0906a6ccf0562354852fae04bc5b52d72a2aff9a871ddb6bd57553569
+	// nginx:latest -> docker.io/library/nginx:latest@sha256:cd5239a0906a6ccf0562354852fae04bc5b52d72a2aff9a871ddb6bd57553569
+
+	// t.Log(reference.FamiliarName(obj))
+	// nginx        -> nginx
+	// nginx:latest -> nginx
+
+	// t.Log(reference.FamiliarString(obj))
+	// nginx        -> nginx@sha256:cd5239a0906a6ccf0562354852fae04bc5b52d72a2aff9a871ddb6bd57553569
+	// nginx:latest -> nginx:latest@sha256:cd5239a0906a6ccf0562354852fae04bc5b52d72a2aff9a871ddb6bd57553569
+}
