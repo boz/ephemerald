9718b1e990694d928b63191f75182578a416094f
Adam Bozanich <adam.boz@gmail.com>
2019-04-16 16:46:04 -0700

    [ci skip] checkpt

--- config/types.go
+++ config/types.go
@@ -1,8 +1,20 @@
 package config
 
 import (
+	"github.com/boz/ephemerald/lifecycle"
+	"github.com/boz/ephemerald/params"
 	"github.com/docker/docker/api/types/strslice"
 )
+
+type Pool struct {
+	Name      string
+	Size      int
+	Image     string
+	Port      int
+	Container *Container
+	Params    params.Config
+	Actions   lifecycle.Config
+}
 
 type Container struct {
 	// docker/docker/api/types/container/config.go
