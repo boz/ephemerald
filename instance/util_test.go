a14670ad336532e281d714493a43cedcabc50939
Adam Bozanich <adam.boz@gmail.com>
2019-04-16 16:51:39 -0700

    [ci skip] checkpt

---
+++ instance/util_test.go
@@ -0,0 +1,26 @@
+package instance
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path"
+	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTCPPorts(t *testing.T) {
+	buf, err := ioutil.ReadFile(path.Join("_testdata", "inspect.postgres.json"))
+	require.NoError(t, err)
+
+	var status types.ContainerJSON
+
+	require.NoError(t, json.Unmarshal(buf, &status))
+
+	ports := tcpPortsFor(status)
+	assert.Equal(t, 1, len(ports))
+
+	assert.Equal(t, "32768", ports["5432"])
+}
--- params/params_test.go
+++ params/params_test.go
@@ -1,16 +1,5 @@
 package params_test
 
-import (
-	"encoding/json"
-	"testing"
-
-	"github.com/boz/ephemerald/params"
-	"github.com/boz/ephemerald/testutil"
-	"github.com/docker/docker/api/types"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-)
-
 // func TestParseConfig(t *testing.T) {
 
 // 	bufs := map[string][]byte{
@@ -26,16 +15,3 @@ import (
 // 		assert.Equal(t, "postgres", cfg.Database, ext)
 // 	}
 // }
-
-func TestTCPPorts(t *testing.T) {
-	buf := testutil.ReadJSON(t, "inspect.postgres.json")
-
-	var status types.ContainerJSON
-
-	require.NoError(t, json.Unmarshal(buf, &status))
-
-	ports := params.TCPPortsFor(status)
-	assert.Equal(t, 1, len(ports))
-
-	assert.Equal(t, "32768", ports["5432"])
-}
