cb5f2e0a79053f2130143dc14e0afbca769288ea
Adam Bozanich <adam.boz@gmail.com>
2019-04-16 16:41:42 -0700

    [ci skip] checkpt

--- config/parse_test.go
+++ config/parse_test.go
@@ -4,8 +4,6 @@ import (
 	"testing"
 
 	"github.com/boz/ephemerald/config"
-	"github.com/boz/ephemerald/testutil"
-	"github.com/boz/ephemerald/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -17,28 +15,17 @@ func TestRead(t *testing.T) {
 }
 
 func doReadTest(t *testing.T, path string, msg string) {
-	log := testutil.Log()
+	cfg := config.Pool{}
 
-	configs, err := config.ReadFile(log, "_testdata/config.json")
+	err := config.ReadFile("_testdata/config.json", &cfg)
 	require.NoError(t, err, msg)
-
-	require.Equal(t, 1, len(configs), msg)
-
-	cfg := configs[0]
 
 	assert.Equal(t, "redis", cfg.Name, msg)
 	assert.Equal(t, "redis", cfg.Image, msg)
 	assert.Equal(t, 6379, cfg.Port, msg)
 	assert.Equal(t, 10, cfg.Size, msg)
 
-	m := cfg.Actions.ForInstance(types.Instance{
-		ID:     testutil.ID(t),
-		PoolID: testutil.ID(t),
-		Port:   cfg.Port,
-		Host:   "127.0.0.1",
-	})
-
-	assert.False(t, m.HasInitialize(), msg)
-	assert.True(t, m.HasHealthcheck(), msg)
-	assert.True(t, m.HasReset(), msg)
+	assert.Nil(t, cfg.Actions.Init, msg)
+	assert.NotNil(t, cfg.Actions.Ready, msg)
+	assert.NotNil(t, cfg.Actions.Reset, msg)
 }
