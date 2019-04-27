6f40a9807b2aeb05c964096f9a279db058727eb2
Adam Bozanich <adam.boz@gmail.com>
2019-04-26 21:11:44 -0700

    [ci skip] checkpt

---
+++ net/client/client.go
@@ -0,0 +1,192 @@
+package client
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"path"
+
+	"github.com/boz/ephemerald/config"
+	"github.com/boz/ephemerald/log"
+	"github.com/boz/ephemerald/types"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	defaultAddress = "localhost:6000"
+	contentType    = "application/json"
+	poolBasePath   = "/pool"
+)
+
+type Interface interface {
+	Pool() PoolInterface
+}
+
+type PoolInterface interface {
+	Create(context.Context, config.Pool) (*types.Pool, error)
+	Get(context.Context, types.ID) (*types.Pool, error)
+	List(context.Context) ([]types.Pool, error)
+	Delete(context.Context, types.ID) error
+
+	Checkout(context.Context, types.ID) (*types.Checkout, error)
+	Release(context.Context, types.ID, types.ID) error
+}
+
+type Opt func(*client) error
+
+func WithHost(host string) Opt {
+	return func(c *client) error {
+		c.host = host
+		return nil
+	}
+}
+
+func WithLog(l logrus.FieldLogger) Opt {
+	return func(c *client) error {
+		c.l = l
+		return nil
+	}
+}
+
+type client struct {
+	host  string
+	chttp *http.Client
+	l     logrus.FieldLogger
+}
+
+func New(opts ...Opt) (Interface, error) {
+	c := &client{
+		host:  defaultAddress,
+		l:     log.Default(),
+		chttp: &http.Client{},
+	}
+
+	for _, opt := range opts {
+		if err := opt(c); err != nil {
+			return nil, err
+		}
+	}
+	return c, nil
+}
+
+func (c *client) Pool() PoolInterface {
+	return c
+}
+
+func (c *client) Create(ctx context.Context, cfg config.Pool) (*types.Pool, error) {
+	buf, err := json.Marshal(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.doRequest(ctx, "POST", poolBasePath, bytes.NewBuffer(buf))
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+	buf, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var pool types.Pool
+	if err := json.Unmarshal(buf, &pool); err != nil {
+		return nil, err
+	}
+	return &pool, nil
+}
+
+func (c *client) Get(ctx context.Context, id types.ID) (*types.Pool, error) {
+	resp, err := c.doRequest(ctx, "GET", path.Join(poolBasePath, string(id)), nil)
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var pool types.Pool
+	if err := json.Unmarshal(buf, &pool); err != nil {
+		return nil, err
+	}
+	return &pool, nil
+}
+
+func (c *client) List(ctx context.Context) ([]types.Pool, error) {
+	resp, err := c.doRequest(ctx, "GET", poolBasePath, nil)
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var pools []types.Pool
+	if err := json.Unmarshal(buf, &pools); err != nil {
+		return nil, err
+	}
+	return pools, nil
+}
+
+func (c *client) Delete(ctx context.Context, id types.ID) error {
+	resp, err := c.doRequest(ctx, "DELETE", path.Join(poolBasePath, string(id)), nil)
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	return err
+}
+
+func (c *client) Checkout(ctx context.Context, id types.ID) (*types.Checkout, error) {
+	resp, err := c.doRequest(ctx, "POST", path.Join(poolBasePath, string(id), "checkout"), nil)
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var obj types.Checkout
+	if err := json.Unmarshal(buf, &obj); err != nil {
+		return nil, err
+	}
+	return &obj, nil
+}
+
+func (c *client) Release(ctx context.Context, pid types.ID, id types.ID) error {
+	resp, err := c.doRequest(ctx, "DELETE", path.Join(poolBasePath, string(pid), "checkout", string(id)), nil)
+	if resp.Body != nil {
+		defer resp.Body.Close()
+	}
+	return err
+}
+
+func (c *client) doRequest(ctx context.Context, method string, path string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, path, body)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	req.Header.Add("Content-Type", contentType)
+
+	return c.chttp.Do(req)
+}
--- net/server.go
+++ net/server.go
@@ -1,223 +1,195 @@
 package net
 
 import (
+	"encoding/json"
+	"errors"
 	"net"
 	"net/http"
-	"strconv"
 
+	"github.com/boz/ephemerald/config"
 	"github.com/boz/ephemerald/poolset"
+	"github.com/boz/ephemerald/types"
 	"github.com/gorilla/mux"
 )
 
-type Server struct {
-	l   *net.TCPListener
-	srv *http.Server
-
-	pset poolset.PoolSet
-
-	closech chan bool
+type Server interface {
+	Address() string
+	Run()
 }
 
-type ServerBuilder struct {
-	address string
-	pset    poolset.PoolSet
-}
+type Opt func(*server) error
 
-func NewServerBuilder() *ServerBuilder {
-	return &ServerBuilder{
-		address: DefaultListenAddress,
+func WithAddress(address string) Opt {
+	return func(s *server) error {
+		s.address = address
+		return nil
 	}
 }
 
-func (sb *ServerBuilder) WithPoolSet(pset poolset.PoolSet) *ServerBuilder {
-	sb.pset = pset
-	return sb
+func WithPoolSet(pset poolset.PoolSet) Opt {
+	return func(s *server) error {
+		s.pset = pset
+		return nil
+	}
 }
 
-func (sb *ServerBuilder) WithAddress(address string) *ServerBuilder {
-	sb.address = address
-	return sb
+type server struct {
+	address string
+	pset    poolset.PoolSet
+
+	l   *net.TCPListener
+	srv *http.Server
 }
 
-func (sb *ServerBuilder) WithPort(port int) *ServerBuilder {
-	address, _, _ := net.SplitHostPort(sb.address)
-	sb.address = net.JoinHostPort(address, strconv.Itoa(port))
-	return sb
-}
+func New(opts ...Opt) (Server, error) {
 
-func (sb *ServerBuilder) Create() (*Server, error) {
-	server := &Server{
-		closech: make(chan bool),
-		pset:    sb.pset,
+	s := &server{
+		address: DefaultListenAddress,
+	}
+
+	for _, opt := range opts {
+		if err := opt(s); err != nil {
+			return nil, err
+		}
+	}
+
+	if s.pset == nil {
+		return nil, errors.New("WithPoolSet required")
 	}
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/pool", server.handlePoolCreate).
+	r.HandleFunc("/pool", s.handlePoolCreate).
 		Methods("POST")
 
-	r.HandleFunc("/pool/{pool-id}/checkout", server.handlePoolInstanceCheckout).
+	r.HandleFunc("/pool/{pool-id}/checkout", s.handlePoolInstanceCheckout).
 		Methods("POST")
 
-	r.HandleFunc("/pool/{pool-id}/checkout/{instance-id}", server.handlePoolInstanceRelease).
+	r.HandleFunc("/pool/{pool-id}/checkout/{instance-id}", s.handlePoolInstanceRelease).
 		Methods("DELETE")
 
-	r.HandleFunc("/pool/{pool-id}", server.handlePoolDelete).
+	r.HandleFunc("/pool/{pool-id}", s.handlePoolDelete).
 		Methods("DELETE")
 
-	l, err := net.Listen("tcp", sb.address)
+	l, err := net.Listen("tcp", s.address)
 	if err != nil {
 		return nil, err
 	}
 
-	server.l = l.(*net.TCPListener)
+	s.l = l.(*net.TCPListener)
 
-	server.srv = &http.Server{
+	s.srv = &http.Server{
 		Handler: r,
 	}
 
-	return server, nil
+	return s, nil
 }
 
-func (s *Server) Run() {
-	defer close(s.closech)
+func (s *server) Run() {
 	s.srv.Serve(s.l)
 }
 
-func (s *Server) Close() {
+func (s *server) Close() {
 	s.l.Close()
 }
 
-func (s *Server) ServerCloseNotify() chan bool {
-	return s.closech
-}
-
-func (s *Server) Address() string {
+func (s *server) Address() string {
 	return s.l.Addr().String()
 }
 
-func (s *Server) Port() int {
-	_, port, _ := net.SplitHostPort(s.Address())
-	p, _ := strconv.Atoi(port)
-	return p
+func (s *server) handlePoolCreate(w http.ResponseWriter, r *http.Request) {
+	var cfg config.Pool
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&cfg); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	pool, err := s.pset.Create(r.Context(), cfg)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	obj := pool.ID() // TODO: Model()
+
+	buf, err := json.Marshal(obj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", rpcContentType)
+	w.Write(buf)
 }
 
-func (s *Server) handlePoolCreate(w http.ResponseWriter, r *http.Request) {
+func (s *server) handlePoolDelete(w http.ResponseWriter, r *http.Request) {
+	pid, ok := mux.Vars(r)["pool-id"]
+	if !ok {
+		http.Error(w, "pool ID required", http.StatusBadRequest)
+		return
+	}
+
+	if err := s.pset.Delete(r.Context(), types.ID(pid)); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
-func (s *Server) handlePoolDelete(w http.ResponseWriter, r *http.Request) {
+
+func (s *server) handlePoolInstanceCheckout(w http.ResponseWriter, r *http.Request) {
+	pid, ok := mux.Vars(r)["pool-id"]
+	if !ok {
+		http.Error(w, "pool ID required", http.StatusBadRequest)
+		return
+	}
+
+	pool, err := s.pset.Get(r.Context(), types.ID(pid))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = pool.Checkout(r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	obj := "TODO: replace with co.model"
+	buf, err := json.Marshal(obj)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", rpcContentType)
+	w.Write(buf)
 }
-func (s *Server) handlePoolInstanceCheckout(w http.ResponseWriter, r *http.Request) {
+
+func (s *server) handlePoolInstanceRelease(w http.ResponseWriter, r *http.Request) {
+
+	pid, ok := mux.Vars(r)["pool-id"]
+	if !ok {
+		http.Error(w, "pool ID required", http.StatusBadRequest)
+		return
+	}
+
+	id, ok := mux.Vars(r)["id"]
+	if !ok {
+		http.Error(w, "Checkout ID required", http.StatusBadRequest)
+		return
+	}
+
+	pool, err := s.pset.Get(r.Context(), types.ID(pid))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := pool.Release(r.Context(), types.ID(id)); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
-func (s *Server) handlePoolInstanceRelease(w http.ResponseWriter, r *http.Request) {
-}
-
-// _, _, err := net.SplitHostPort(r.Host)
-// if err != nil {
-// 	http.Error(w, fmt.Sprint(err), http.StatusBadRequest)
-// 	return
-// }
-
-// // ps, err := s.pools.CheckoutWith(r.Context())
-
-// // for name, p := range ps {
-// // 	// p2, e := p.ForHost(host)
-// // 	// if e != nil {
-// // 	// 	err = e
-// // 	// 	break
-// // 	// }
-// // 	// ps[name] = p2
-// // }
-
-// if err != nil {
-// 	// s.pools.ReturnAll(ps)
-// 	// http.Error(w, fmt.Sprint(err), http.StatusRequestTimeout)
-// 	return
-// }
-
-// // buf, err := json.Marshal(ps)
-// if err != nil {
-// 	http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-// 	s.pools.ReturnAll(ps)
-// 	return
-// }
-
-// w.Header().Set("Content-Type", rpcContentType)
-// w.Write(buf)
-
-// func (s *Server) handleCheckoutPool(w http.ResponseWriter, r *http.Request) {
-// host, _, err := net.SplitHostPort(r.Host)
-// if err != nil {
-// 	http.Error(w, fmt.Sprint(err), http.StatusBadRequest)
-// 	return
-// }
-
-// poolName := mux.Vars(r)["pool"]
-// if poolName == "" {
-// 	http.Error(w, "Invalid pool name", http.StatusBadRequest)
-// 	return
-// }
-
-// ps, err := s.pools.CheckoutWith(r.Context(), poolName)
-// if err != nil {
-// 	s.pools.ReturnAll(ps)
-// 	http.Error(w, fmt.Sprint(err), http.StatusRequestTimeout)
-// 	return
-// }
-
-// params, ok := ps[poolName]
-// if !ok {
-// 	s.pools.ReturnAll(ps)
-// 	http.Error(w, "Pool not found", http.StatusInternalServerError)
-// 	return
-// }
-
-// params, err = params.ForHost(host)
-// if err != nil {
-// 	s.pools.ReturnAll(ps)
-// 	http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-// 	return
-// }
-
-// buf, err := json.Marshal(params)
-// if err != nil {
-// 	s.pools.ReturnAll(ps)
-// 	http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-// 	return
-// }
-// w.Header().Set("Content-Type", rpcContentType)
-// w.Write(buf)
-// }
-
-// func (s *Server) handleReturnBatch(w http.ResponseWriter, r *http.Request) {
-// ps := params.Set{}
-
-// dec := json.NewDecoder(r.Body)
-
-// if err := dec.Decode(&ps); err != nil {
-// 	http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-// 	return
-// }
-// s.pools.ReturnAll(ps)
-
-// w.Header().Set("Content-Type", rpcContentType)
-// w.WriteHeader(http.StatusOK)
-// }
-
-// func (s *Server) handleReturn(w http.ResponseWriter, r *http.Request) {
-// pool := mux.Vars(r)["pool"]
-// if pool == "" {
-// 	http.Error(w, "Invalid pool name", http.StatusBadRequest)
-// 	return
-// }
-
-// id := mux.Vars(r)["id"]
-// if id == "" {
-// 	http.Error(w, "Invalid ID", http.StatusBadRequest)
-// 	return
-// }
-
-// s.pools.Return(pool, itemID(id))
-
-// w.Header().Set("Content-Type", rpcContentType)
-// w.WriteHeader(http.StatusOK)
-// }
